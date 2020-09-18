5d2ee022e71f9b6976edac1f5f647bec1b26bd36
getromandev <getroman.dev@gmail.com>
2020-09-18 18:30:53 -0400

    add sidebar components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-
+import Sidebar from './Sidebar';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Header />
 
       <div className='app_body'>
-        {/* Sidebar  */}
+        <Sidebar />
         {/* Feed  */}
         {/* Widgets  */}
       </div>  
