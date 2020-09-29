39198601ece7a7048924469ac4206ac605522e15
getromandev <getroman.dev@gmail.com>
2020-09-29 13:38:16 -0400

    set up login component and connect firebase DB

--- src/App.js
+++ src/App.js
@@ -2,22 +2,31 @@ import React from 'react';
 import './App.css';
 import Feed from './Feed';
 import Header from './Header';
+import Login from './Login';
 import Sidebar from './Sidebar';
 import Widgets from './Widgets'
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [ { user }, dispatch] = useStateValue();
+  
   return (
     // BEM naming convention
     <div className="app">
+      {!user ? (
+        <Login /> 
+        ) : (
+        <>
+          <Header />
 
-      <Header />
-
-      <div className='app__body'>
-        <Sidebar />
-        <Feed />
-        <Widgets />
-      </div>  
-    </div>
+          <div className='app__body'>
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      )}
+    </div>  
   );
 }
 
