956341e0e3ae3b19a1fd4dc55afb3ae025b10e6d
getromandev <getroman.dev@gmail.com>
2020-10-01 21:32:33 -0400

    add post functionality connect firebase DB to handle posts submit

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -5,6 +5,8 @@ import VideocamIcon from '@material-ui/icons/Videocam';
 import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import { useStateValue } from './StateProvider';
+import db from './firebase';
+import firebase from 'firebase';
 
 function MessageSender() {
     const [{ user }, dispatch] = useStateValue();
@@ -15,6 +17,13 @@ function MessageSender() {
         e.preventDefault();
 
         // some DB stuff happens here
+        db.collection('posts').add({
+            message: input,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+            profilePic: user.photoURL,
+            username: user.displayName,
+            image: imageURL
+        })
 
         setInput('')
         setImageURL('')
